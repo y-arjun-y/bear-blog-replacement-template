ecd846ed91daed9ef89fffc09e0b30bcbacab597
y-arjun-y <arjyadav7@gmail.com>
2022-08-08 09:51:41 +0400

    changed favicon and added comment

--- pages/_document.js
+++ pages/_document.js
@@ -4,9 +4,10 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        {/* Change "🐻" to any Unicode character you like, it'll show up as the favicon for all pages. */}
         <link
           rel="shortcut icon"
-          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>A</text></svg>"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🐻</text></svg>"
         ></link>
         {/* Change "https://unpkg.com/beech.css@1.0.9/style.css" to any classless CSS CDN link you want! */}
         <link
