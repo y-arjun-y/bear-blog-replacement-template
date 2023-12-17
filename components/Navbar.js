85cb824a8d7405be23b89d5efe4fdc8a692276e7
Owen Dobson <63920494+otdobson@users.noreply.github.com>
2023-12-16 22:42:35 -0500

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,14 +2,12 @@ export default function Navbar() {
   return (
     <header>
       <a href="/">
-        <h1>Bear Blog Replacement Template</h1>
+        <h1>Owen Dobson</h1>
       </a>
       <nav>
         <p>
           <a href="/">Home</a>
           <a href="/blog/">Blog</a>
-          <a href="/the-grizzly-bear-chill-zone/">
-            The Grizzly Bear Chill Zone
           </a>
         </p>
       </nav>
