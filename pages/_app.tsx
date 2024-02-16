908e83ddefa8f17af86645dd2da2a3447aa81622
y-arjun-y <arjyadav7@gmail.com>
2024-02-16 23:04:07 +0400

    re-commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -24,7 +24,7 @@ function BearBlogReplacementTemplate({ Component, pageProps }) {
         />
         <meta
           property="og:site_name"
-          content="Bear Blog Replacement Template Demo"
+          content="Bear Blog Replacement Template"
         />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
